641eb7f60996dcacbef024eed92ffff543b88d1f
21120447 <haonhat2729@gmail.com>
2024-07-22 22:34:07 +0700

    feat: initial project

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Welcome to the homepage</h1>
+    <AppAlert> This is an auto-imported component </AppAlert>
+  </div>
+</template>
+
+<script setup lang="ts">
+useHead({
+  title: "My App",
+  meta: [{ name: "description", content: "My amazing site." }],
+  bodyAttrs: {
+    class: "test",
+  },
+  script: [{ innerHTML: "console.log('Hello world')" }],
+});
+</script>
