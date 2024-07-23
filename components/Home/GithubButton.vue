3be02fe13797ca95e434d56bb53ae1d100e97b96
21120447 <haonhat2729@gmail.com>
2024-07-23 23:47:09 +0700

    :recycle: refactor: change github button to top

--- components/Home/GithubButton.vue
+++ components/Home/GithubButton.vue
@@ -2,7 +2,7 @@
   <NuxtLink
     to="https://github.com/nxhawk"
     target="blank"
-    class="animate-pulse absolute right-0 bottom-12 flex rounded-l-full justify-center items-center gap-1 z-50 w-fit h-fit p-2 shadow-md border-y border-l border-black hover:bg-primary hover:text-white hover:animate-none"
+    class="animate-pulse absolute right-0 top-24 flex rounded-l-full justify-center items-center gap-1 z-50 w-fit h-fit p-2 shadow-md border-y border-l border-black hover:bg-primary hover:text-white hover:animate-none"
   >
     <CommonIcon name="Github" :stroke-width="2" />
     <span className="font-rubik text-2xl max-sm:text-xl">Github</span>
