65ec7619f3ee2fa6de8268ee627ba96a8160f0e1
21120447 <haonhat2729@gmail.com>
2024-07-23 17:55:47 +0700

    :sparkles: feat: navbar for app

--- components/ui/tooltip/TooltipContent.vue
+++ components/ui/tooltip/TooltipContent.vue
@@ -13,6 +13,7 @@ defineOptions({
   inheritAttrs: false,
 });
 
+// eslint-disable-next-line vue/require-default-prop
 const props = withDefaults(defineProps<TooltipContentProps & { class?: HTMLAttributes["class"] }>(), {
   sideOffset: 4,
 });
