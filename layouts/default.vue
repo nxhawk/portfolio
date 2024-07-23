a2a0b09d9583c9cc0f517f3d02240665afab3cad
21120447 <haonhat2729@gmail.com>
2024-07-23 23:32:16 +0700

    :recycle: refactor: main content position

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <main
-    class="flex relative screen break-words min-h-screen items-center justify-between pt-20 pb-4 px-32 max-md:p-4 bg-transparent max-sm:pt-20 bg-[radial-gradient(#2f7df4_1px,transparent_1px)] [background-size:16px_16px]"
+    class="flex relative screen break-words items-center justify-between pt-20 pb-4 px-32 max-md:p-4 bg-transparent max-sm:pt-20 bg-[radial-gradient(#2f7df4_1px,transparent_1px)] [background-size:16px_16px]"
   >
     <LayoutNavbar />
     <slot />
