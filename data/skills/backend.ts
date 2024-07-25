a70e9c2627a1f0fe6afee3c8d578f53bc76c6202
21120447 <haonhat2729@gmail.com>
2024-07-25 12:52:47 +0700

    :truck: chore: move resources official logo image

--- data/skills/backend.ts
+++ data/skills/backend.ts
@@ -4,35 +4,35 @@ const backend: ISkill[] = [
   {
     alt: "node",
     img: "/skills/backend/nodejs-icon.svg",
-    official: "/skills/backend/official/nodejs-icon-official.svg",
+    official: "/skills/backend/nodejs-icon-official.svg",
     description:
       "Node.js is open-source: This means that the source code for Node.js is publicly available. And it's maintained by contributors from all over the world. The Node.js contribution guide shows you how to contribute. Node.js is cross-platform: Node.js is not dependent on any operating system software. It can work on Linux, macOS, or Windows. Node.js is a JavaScript runtime environment: When you write JavaScript code in your text editor, that code cannot perform any task unless you execute (or run) it. And to run your code, you need a runtime environment.",
   },
   {
     alt: "nestjs",
     img: "/skills/backend/nestjs-icon.svg",
-    official: "/skills/backend/official/nestjs-icon-official.svg",
+    official: "/skills/backend/nestjs-icon-official.svg",
     description:
       "Nest (NestJS) is a framework for building efficient, scalable Node.js server-side applications. It uses progressive JavaScript, is built with and fully supports TypeScript (yet still enables developers to code in pure JavaScript) and combines elements of OOP (Object Oriented Programming), FP (Functional Programming), and FRP (Functional Reactive Programming).",
   },
   {
     alt: "spring",
     img: "/skills/backend/springio-icon.svg",
-    official: "/skills/backend/official/springio-icon-official.svg",
+    official: "/skills/backend/springio-icon-official.svg",
     description:
       'The Spring Framework provides a comprehensive programming and configuration model for modern Java-based enterprise applications - on any kind of deployment platform. A key element of Spring is infrastructural support at the application level: Spring focuses on the "plumbing" of enterprise applications so that teams can focus on application-level business logic, without unnecessary ties to specific deployment environments.',
   },
   {
     alt: "django",
     img: "/skills/backend/djangoproject-icon.svg",
-    official: "/skills/backend/official/djangoproject-icon-official.svg",
+    official: "/skills/backend/djangoproject-icon-official.svg",
     description:
       "Django is a high-level Python web framework that enables rapid development of secure and maintainable websites. Built by experienced developers, Django takes care of much of the hassle of web development, so you can focus on writing your app without needing to reinvent the wheel. It is free and open source, has a thriving and active community, great documentation, and many options for free and paid-for support.",
   },
   {
     alt: "flask",
     img: "/skills/backend/pocoo_flask-icon.svg",
-    official: "/skills/backend/official/pocoo_flask-icon-official.svg",
+    official: "/skills/backend/pocoo_flask-icon-official.svg",
     description:
       "Flask is a web framework, it’s a Python module that lets you develop web applications easily. It’s has a small and easy-to-extend core: it’s a microframework that doesn’t include an ORM (Object Relational Manager) or such features. It does have many cool features like url routing, template engine. It is a WSGI web app framework.",
   },
--- data/skills/tools.ts
+++ data/skills/tools.ts
@@ -4,70 +4,70 @@ const tools: ISkill[] = [
   {
     alt: "git",
     img: "/skills/tools/git-scm-icon.svg",
-    official: "/skills/tools/official/git-scm-icon-official.svg",
+    official: "/skills/tools/git-scm-icon-official.svg",
     description:
       "Git is a free and open source distributed version control system designed to handle everything from small to very large projects with speed and efficiency. Git is easy to learn and has a tiny footprint with lightning fast performance. It outclasses SCM tools like Subversion, CVS, Perforce, and ClearCase with features like cheap local branching, convenient staging areas, and multiple workflows.",
   },
   {
     alt: "visual studio code",
     img: "/skills/tools/visualstudio_code-icon.svg",
-    official: "/skills/tools/official/visualstudio_code-icon-official.svg",
+    official: "/skills/tools/visualstudio_code-icon-official.svg",
     description:
       "Visual Studio Code, also commonly referred to as VS Code, is a source-code editor developed by Microsoft for Windows, Linux, macOS and web browsers.Features include support for debugging, syntax highlighting, intelligent code completion, snippets, code refactoring, and embedded version control with Git. Users can change the theme, keyboard shortcuts, preferences, and install extensions that add functionality.",
   },
   {
     alt: "jetbrains",
     img: "/skills/tools/jetbrains-icon.svg",
-    official: "/skills/tools/official/jetbrains-icon-official.svg",
+    official: "/skills/tools/jetbrains-icon-official.svg",
     description:
       "JetBrains is a cutting-edge software vendor specializing in the creation of intelligent development tools, including IntelliJ IDEA – the leading Java IDE, and the Kotlin programming language. Essential tools for software developers and teams.",
   },
   {
     alt: "trello",
     img: "/skills/tools/trello-icon.svg",
-    official: "/skills/tools/official/trello-icon-official.svg",
+    official: "/skills/tools/trello-icon-official.svg",
     description:
       "Trello is the visual tool that empowers your team to manage any type of project, workflow, or task tracking. Add files, checklists, or even automation: Customize it all for how your team works best. Just sign up, create a board, and you’re off!",
   },
   {
     alt: "postman",
     img: "/skills/tools/getpostman-icon.svg",
-    official: "/skills/tools/official/getpostman-icon-official.svg",
+    official: "/skills/tools/getpostman-icon-official.svg",
     description:
       "Postman is an API development and testing platform that offers a variety of features. It allows developers to easily create and share API requests and collections, automate testing, mock APIs, and monitor performance. Postman also provides collaboration and documentation tools, enabling teams to work together more efficiently and communicate API behavior effectively.",
   },
   {
     alt: "linux",
     img: "/skills/tools/linux-icon.svg",
-    official: "/skills/tools/official/linux-icon-official.svg",
+    official: "/skills/tools/linux-icon-official.svg",
     description:
       "Just like Windows, iOS, and Mac OS, Linux is an operating system. In fact, one of the most popular platforms on the planet, Android, is powered by the Linux operating system. An operating system is software that manages all of the hardware resources associated with your desktop or laptop. To put it simply, the operating system manages the communication between your software and your hardware. Without the operating system (OS), the software wouldn’t function.",
   },
   {
     alt: "firebase",
     img: "/skills/tools/firebase-icon.svg",
-    official: "/skills/tools/official/firebase-icon-official.svg",
+    official: "/skills/tools/firebase-icon-official.svg",
     description:
       "Firebase is a cloud services provider and backend as a service company. Firebase can power your app's backend, including data storage, user authentication, static hosting, and more. Firebase provides these services so you can focus on creating extraordinary user experiences.",
   },
   {
     alt: "figma",
     img: "/skills/tools/figma-icon.svg",
-    official: "/skills/tools/official/figma-icon-official.svg",
+    official: "/skills/tools/figma-icon-official.svg",
     description:
       "Figma is a powerful design tool that helps you to create anything: websites, applications, logos, and much more.",
   },
   {
     alt: "jenkins",
     img: "/skills/tools/jenkins-icon.svg",
-    official: "/skills/tools/official/jenkins-icon-official.svg",
+    official: "/skills/tools/jenkins-icon-official.svg",
     description:
       "Jenkins is an open source continuous integration (CI) server. It manages and controls several stages of the software delivery process, including build, documentation, automated testing, packaging, and static code analysis. Jenkins is a highly popular DevOps tool used by thousands of development teams.",
   },
   {
     alt: "docker",
     img: "/skills/tools/docker-icon.svg",
-    official: "/skills/tools/official/docker-icon-official.svg",
+    official: "/skills/tools/docker-icon-official.svg",
     description:
       "Docker is an open platform for developing, shipping, and running applications. Docker enables you to separate your applications from your infrastructure so you can deliver software quickly. With Docker, you can manage your infrastructure in the same ways you manage your applications. By taking advantage of Docker's methodologies for shipping, testing, and deploying code, you can significantly reduce the delay between writing code and running it in production.",
   },
