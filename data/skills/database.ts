46801ddf9e4d72a152ab9ac793a49c037a1678b0
21120447 <haonhat2729@gmail.com>
2024-07-25 12:44:27 +0700

    :truck: chore: rename official logo image

--- data/skills/database.ts
+++ data/skills/database.ts
@@ -4,35 +4,35 @@ const database: ISkill[] = [
   {
     alt: "mysql",
     img: "/skills/database/mysql-icon.svg",
-    official: "/skills/database/official/mysql-icon.svg",
+    official: "/skills/database/official/mysql-icon-official.svg",
     description:
       "MySQL is the world’s most popular open source database. According to DB-Engines, MySQL ranks as the second-most-popular database, behind Oracle Database. MySQL powers many of the most accessed applications, including Facebook, Twitter, Netflix, Uber, Airbnb, Shopify, and Booking.com.",
   },
   {
     alt: "postgresql",
     img: "/skills/database/postgresql-icon.svg",
-    official: "/skills/database/official/postgresql-icon.svg",
+    official: "/skills/database/official/postgresql-icon-official.svg",
     description:
       "PostgreSQL is a powerful, open source object-relational database system that uses and extends the SQL language combined with many features that safely store and scale the most complicated data workloads. The origins of PostgreSQL date back to 1986 as part of the POSTGRES project at the University of California at Berkeley and has more than 35 years of active development on the core platform.",
   },
   {
     alt: "mongodb",
     img: "/skills/database/mongodb-icon.svg",
-    official: "/skills/database/official/mongodb-icon.svg",
+    official: "/skills/database/official/mongodb-icon-official.svg",
     description:
       "MongoDB is a document database designed for ease of application development and scaling. You can run MongoDB in the following environments:\nMongoDB Atlas: The fully managed service for MongoDB deployments in the cloud.\nMongoDB Enterprise: The subscription-based, self-managed version of MongoDB.\nMongoDB Community: The source-available, free-to-use, and self-managed version of MongoDB",
   },
   {
     alt: "SQLite",
     img: "/skills/database/sqlite-icon.svg",
-    official: "/skills/database/official/sqlite-icon.svg",
+    official: "/skills/database/official/sqlite-icon-official.svg",
     description:
       "SQLite is a C-language library that implements a small, fast, self-contained, high-reliability, full-featured, SQL database engine. SQLite is the most used database engine in the world. SQLite is built into all mobile phones and most computers and comes bundled inside countless other applications that people use every day",
   },
   {
     alt: "redis",
     img: "/skills/database/redis-icon.svg",
-    official: "/skills/database/official/redis-icon.svg",
+    official: "/skills/database/official/redis-icon-official.svg",
     description:
       "Redis (REmote DIctionary Server) is an open source, in-memory, NoSQL key/value store that is used primarily as an application cache or quick-response database. Redis stores data in memory, rather than on a disk or solid-state drive (SSD), which helps deliver unparalleled speed, reliability, and performance.",
   },
--- data/skills/language.ts
+++ data/skills/language.ts
@@ -4,42 +4,42 @@ const language: ISkill[] = [
   {
     alt: "javascript",
     img: "/skills/language/javascript-original.svg",
-    official: "/skills/language/official/javascript-original.svg",
+    official: "/skills/language/official/javascript-original-official.svg",
     description:
       "JavaScript is a scripting language that enables you to create dynamically updating content, control multimedia, animate images, and pretty much everything else.",
   },
   {
     alt: "typescript",
     img: "/skills/language/typescript-original.svg",
-    official: "/skills/language/official/typescript-original.svg",
+    official: "/skills/language/official/typescript-original-official.svg",
     description:
       "TypeScript is a strongly typed programming language that builds on JavaScript, giving you better tooling at any scale.",
   },
   {
     alt: "c++",
     img: "/skills/language/cplusplus-original.svg",
-    official: "/skills/language/official/cplusplus-original.svg",
+    official: "/skills/language/official/cplusplus-original-official.svg",
     description:
       "C++ is a cross-platform language that can be used to create high-performance applications. C++ was developed by Bjarne Stroustrup, as an extension to the C language. C++ gives programmers a high level of control over system resources and memory.",
   },
   {
     alt: "java",
     img: "/skills/language/java-original.svg",
-    official: "/skills/language/official/java-original.svg",
+    official: "/skills/language/official/java-original-official.svg",
     description:
       "Java is a programming language and computing platform first released by Sun Microsystems in 1995. It has evolved from humble beginnings to power a large share of today’s digital world, by providing the reliable platform upon which many services and applications are built. New, innovative products and digital services designed for the future continue to rely on Java, as well.",
   },
   {
     alt: "python",
     img: "/skills/language/python-original.svg",
-    official: "/skills/language/official/python-original.svg",
+    official: "/skills/language/official/python-original-official.svg",
     description:
       "Python is an interpreted, object-oriented, high-level programming language with dynamic semantics. Its high-level built in data structures, combined with dynamic typing and dynamic binding, make it very attractive for Rapid Application Development, as well as for use as a scripting or glue language to connect existing components together.",
   },
   {
     alt: "golang",
     img: "/skills/language/brand-golang.svg",
-    official: "/skills/language/official/brand-golang.svg",
+    official: "/skills/language/official/brand-golang-official.svg",
     description:
       "Go, also called Golang or Go language, is an Open Source programming language that Google developed. Software developers use Go in an array of operating systems and frameworks to develop web applications, cloud and networking services, and other types of software.",
   },
